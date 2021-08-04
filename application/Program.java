d46d1286bceb877a08213559be9b04c3812cb6de
viniarnhold <viniciusarnh@gmail.com>
2021-08-04 14:35:36 -0300

    Update para o SHP
    #1

--- application/Program.java
+++ application/Program.java
@@ -4,6 +4,7 @@ public class Program {
     public static void main(String[] args){
 
         System.out.println("Hello World");
+        System.out.println("Welcome to the Super Heroes Program");
 
     }
     
